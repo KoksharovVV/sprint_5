4c8a35343cbe15b34447a1921cbce7ba1ea74014
Koksharov <vladimircoksharoff@yandex.ru>
2024-05-27 21:27:11 +0500

    create import

---
+++ tests/test_authorization_page.py
@@ -0,0 +1,10 @@
+import pytest
+import random
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+class TestAuthorizationPage:
+    pass
--- tests/test_registrarion_page.py
+++ tests/test_registrarion_page.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-class test_registrarion_page():
 
-    def test_registrarion_(self):
-        pass
 
+class TestRegistrarionPage():
+    pass
