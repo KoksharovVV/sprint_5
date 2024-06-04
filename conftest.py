bd8f42c5ae5f0cc4cdf9e92aa303bd083f04fdb6
Koksharov <vladimircoksharoff@yandex.ru>
2024-06-05 01:02:50 +0500

    Create import webdriver-manager

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import random
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -7,7 +8,7 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture(scope='function')
 def driver():
     options = webdriver.ChromeOptions()
-    service = Service(executable_path="driver/chromedriver.exe")
+    service = Service(executable_path=ChromeDriverManager().install())
     options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
